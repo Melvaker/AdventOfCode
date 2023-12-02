bc26ba763135e2283b6e9c4aa48cc53e9269b3a9
Melvaker <47873766+Melvaker@users.noreply.github.com>
2023-12-01 22:45:32 -0500

    2023 Prep
    Prepared files for 2023 advent of code.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿using System;
-
-namespace AdventOfCode
+﻿namespace AdventOfCode
 {
     class Program
     {
         static void Main()
         {
             //AoC2015.RunDayFour();
-            AoC2021.RunDayEight();
+            //AoC2021.RunDayEight();
+            AoC2023.RunDayOne();
         }
     }
 }
---
+++ Years/AoC2023.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Years
+{
+    internal class AoC2023
+    {
+        public static void RunDayOne()
+        {
+
+        }
+
+        private static void DayOne()
+        {
+            
+        }
+    }
+}
